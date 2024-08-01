ba5364e17d644c3e91308152e8a5873f9ae65ab6
TC\anilc <can.anil@technicolor.com>
2024-08-01 23:41:16 +0100

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+AD_HOST_NAME = 'al-local.herr.local'
+AD_DOMAIN_NAME = 'ad.herr.local'
+
+AD_AUTH_GROUP_CHECK = False
+AD_AUTH_GROUP_NAME = '*Global Data Analytics'
+
+WEBSERVER_ADDRESS = '0.0.0.0'
+WEBSERVER_PORT = 5000
+
+MAGIC_PW = None
